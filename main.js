930e558fa1851a2f11e5efae4785c775957c38c1
imrankhan5523 <163529781+imrankhan5523@users.noreply.github.com>
2024-03-22 15:25:19 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var personName = "    \tMuhammad Imran Khan\n   ";
+console.log("Name with whitespace:\"".concat(personName, "\""));
+var strippedName = personName.trim();
+console.log("stripped Name: \"".concat(strippedName, "\""));
